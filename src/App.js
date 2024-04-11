cb42b0e66527675a846fbd893dde93b0fcd5d43b
markuspalu <palu1@stolaf.edu>
2024-04-11 17:33:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+import voidwaker from './images/voidwaker.png';
+import ghraziRapier from './images/ghraziRapier.png';
+import scytheOfVitur from './images/scytheOfVitur.png';
+import abyssalWhip from './images/abyssalWhip.png';
+import saradominSword from './images/saradominSword.png';
+import dragonScimitar from './images/dragonScimitar.png';
+import runeScimitar from './images/runeScimitar.png';
+import dragonDagger from './images/dragonDagger.png';
+import graniteMaul from './images/graniteMaul.png';
+import bronzeScimitar from './images/bronzeScimitar.png';
+import ironScimitar from './images/ironScimitar.png';
+import steelScimitar from './images/steelScimitar.png';
+
+
+const items = {
+  "itemSelection": {
+    "Gold": {
+      "items": ["Voidwaker", "Ghrazi Rapier", "Scythe of vitur"],
+      "color": "bg-yellow-300"
+    },
+    "Red": {
+      "items": ["Abyssal Whip", "Saradomin Sword", "Dragon Scimitar"],
+      "color": "bg-red-300"
+    },
+    "Purple": {
+      "items": ["Rune Scimitar", "Dragon Dagger", "Granite Maul"],
+      "color": "bg-purple-300"
+    },
+    "Blue": {
+      "items": ["Bronze Scimitar", "Iron Scimitar", "Steel Scimitar"],
+      "color": "bg-blue-300"
+    }
+  },
+  "srcSelection": {
+    "Voidwaker": voidwaker,
+    "Ghrazi Rapier": ghraziRapier,
+    "Scythe of vitur": scytheOfVitur,
+    "Abyssal Whip": abyssalWhip,
+    "Saradomin Sword": saradominSword,
+    "Dragon Scimitar": dragonScimitar,
+    "Rune Scimitar": runeScimitar,
+    "Dragon Dagger": dragonDagger,
+    "Granite Maul": graniteMaul,
+    "Bronze Scimitar": bronzeScimitar,
+    "Iron Scimitar": ironScimitar,
+    "Steel Scimitar": steelScimitar
+  }
+}
+
+const Inventory = ({ itemList }) => {
+  return (
+    <div className='my-12 px-24'>
+      <h2 className='font-bold'>Inventory:</h2>
+      <div className='flex gap-4 flex-wrap'>
+        {itemList.map((item, index) => {
+          const [color] = Object.entries(items.itemSelection).find(([color, { items }]) => items.includes(item));
+          const colorClass = items.itemSelection[color].color;
+          const imageSrc = items.srcSelection[item];
+          return (
+            <div className={`${colorClass} p-2 border border-gray-400 rounded-lg flex flex-col justify-center items-center`} key={index}>
+              <p>{item}</p>
+              <img className={"w-auto h-16"} src={imageSrc} alt={`OSRS img of ${imageSrc}`}></img>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+
+const Case = ( {openedItem, setOpenedItem, openedColor, setOpenedColor, setItemList} ) => {
+
+  const selectItem = (color) => {
+    let chanceItem = Math.floor(Math.random() * 3); // change size dynamically to object length
+    const selectedItem = items.itemSelection[color].items[chanceItem];
+    setOpenedItem(selectedItem);
+      setOpenedColor(items.itemSelection[color]);
+    setItemList(prevList => {
+      const newList = [...prevList, selectedItem];
+      return newList;
+    });
+  }
+
+  const openCase = () => {    
+    setTimeout(() => {
+      const chance = Math.floor(Math.random() * 1000);
+      if (chance <= 2) {
+        selectItem("Gold");
+      } else if (chance > 2 && chance <= 12) {
+        selectItem("Red");
+      } else if (chance > 12 && chance <= 62) {
+        selectItem("Purple");
+      } else {
+        selectItem("Blue");
+      }
+    }, "2000");
+  }
+
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <button onClick={openCase} className='w-24 bg-green-400 p-4'>Open</button>
+      {openedItem &&
+        <div className={`${openedColor} p-2`} >
+          <h2>You opened:</h2>
+          <b>{openedItem}</b>
+        </div>
+      }
+    </div>
+  )
+}
+
+const Opening = () => {
+  const fakeList = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10", 
+                    "item11", "item12", "item13", "item14", "item15", "item16", "item17", "item18", "item19", "item20",
+                    "item21", "item22", "item23", "item24", "item25", "item26", "item27", "item28", "item29", "item30"];
+  const [isOpen, setIsOpen] = useState(false);
+
+
+  const openCase = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 8000)
+  }
+
+  return (
+    <>
+    <div className='scroller-container'>
+      <div className={`scroller ${isOpen ? 'open' : ''}`}>
+        {fakeList.map((item) => (
+          <div className="p-2 border border-gray-400 rounded-lg" key={item}>{item}</div>
+        ))}
+      </div>
+    </div>
+    <button onClick={openCase}>Roll</button>
+    </>
+  );
+}
 
 function App() {
+
+  const [itemList, setItemList] = useState([]);
+  const [openedItem, setOpenedItem] = useState("");
+  const [openedColor, setOpenedColor] = useState("");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="flex flex-col items-center">
+      <Case openedColor={openedColor} setOpenedColor={setOpenedColor} 
+            openedItem={openedItem} setOpenedItem={setOpenedItem}
+            itemList={itemList} setItemList={setItemList} />
+      <Opening />
+    </div>
+      <Inventory itemList={itemList}/>
     </div>
   );
 }
+
+
+
 
 export default App;
